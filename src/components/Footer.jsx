36f8c3ac767d346fef0bdfce2522674a88cf7bab
Himanshu Tomar <myjobportal2001@gmail.com>
2022-12-26 18:45:02 +0530

    My project files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/himanshu-tomar-77a108138/" target={"blank"}>
+            linkedin
+          </a>
+          <a href="https://instagram.com/himanshu_987654321" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/himanshu-mygithub/Myprojects" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
